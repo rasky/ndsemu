74a1794c3348d6b775dd91be7a820ba07ce931fa
Giovanni Bajo <rasky@develer.com>
2015-12-15 19:33:45 +0100

    irq: implement initial irq triggering

--- irq.go
+++ irq.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"ndsemu/arm"
+
 	log "gopkg.in/Sirupsen/logrus.v0"
 )
 
 type HwIrq struct {
-	master uint32
+	Cpu    *arm.Cpu
+	master uint16
 	enable uint32
-	ack    uint32
+	flags  uint32
 }
 
-func (irq *HwIrq) ReadIME() uint32 {
+type IrqType uint32
+
+const (
+	IrqHBlank IrqType = (1 << 0)
+	IrqVBlank IrqType = (1 << 1)
+	IrqVMatch IrqType = (1 << 2)
+	IrqTimer0 IrqType = (1 << 3)
+	IrqTimer1 IrqType = (1 << 4)
+	IrqTimer2 IrqType = (1 << 5)
+	IrqTimer3 IrqType = (1 << 6)
+
+	IrqGameCard IrqType = (1 << 19)
+)
+
+func (irq *HwIrq) ReadIME() uint16 {
 	return irq.master
 }
 
@@ -19,19 +36,33 @@ func (irq *HwIrq) ReadIE() uint32 {
 }
 
 func (irq *HwIrq) ReadIF() uint32 {
-	return irq.ack
+	return irq.flags
 }
 
-func (irq *HwIrq) WriteIME(ime uint32) {
+func (irq *HwIrq) updateLineStatus() {
+	irqstat := irq.master != 0 && (irq.enable&irq.flags) != 0
+	if irqstat {
+		log.Infof("[irq] trigger %08x", irq.flags&irq.enable)
+	}
+	irq.Cpu.SetLine(arm.LineIrq, irqstat)
+}
+
+func (irq *HwIrq) WriteIME(ime uint16) {
 	irq.master = ime & 1
-	log.Infof("[irq] IME:%x", irq.master)
+	irq.updateLineStatus()
 }
 
 func (irq *HwIrq) WriteIE(ie uint32) {
 	irq.enable = ie
-	log.Infof("[irq] IE:%x", irq.enable)
+	irq.updateLineStatus()
 }
 
 func (irq *HwIrq) WriteIF(ifx uint32) {
-	log.Infof("[irq] IF:%x", ifx)
+	irq.flags &^= ifx
+	irq.updateLineStatus()
+}
+
+func (irq *HwIrq) Raise(irqtype IrqType) {
+	irq.flags |= uint32(irqtype)
+	irq.updateLineStatus()
 }
