2f452cbb2d45f4f05bcdf98b3b6ecf6e7ac8cd1d
Giovanni Bajo <rasky@develer.com>
2015-12-16 16:51:49 +0100

    ipc: silent

--- ipc.go
+++ ipc.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	log "gopkg.in/Sirupsen/logrus.v0"
-)
-
 type HwIpc struct {
 	data [2]uint16
 }
@@ -15,18 +9,18 @@ func (ipc *HwIpc) WriteIPCSYNC(cpunum CpuNum, value uint16) {
 	if value&(1<<13) != 0 || value&(1<<14) != 0 {
 		panic("not implemented: IPCSYNC IRQ emulation")
 	}
-	log.WithFields(log.Fields{
-		"cpu":   cpunum,
-		"value": fmt.Sprintf("%04x", ipc.data[cpunum]),
-	}).Info("[IPC] Write sync")
+	// logrus.WithFields(logrus.Fields{
+	// 	"cpu":   cpunum,
+	// 	"value": fmt.Sprintf("%04x", ipc.data[cpunum]),
+	// }).Info("[IPC] Write sync")
 }
 
 func (ipc *HwIpc) ReadIPCSYNC(cpunum CpuNum) uint16 {
 	value := ipc.data[cpunum]
 	value = (value &^ 0xF) | ((ipc.data[1-cpunum] >> 8) & 0xF)
-	log.WithFields(log.Fields{
-		"cpu":   cpunum,
-		"value": fmt.Sprintf("%04x", value),
-	}).Info("[IPC] Read sync")
+	// logrus.WithFields(logrus.Fields{
+	// 	"cpu":   cpunum,
+	// 	"value": fmt.Sprintf("%04x", value),
+	// }).Info("[IPC] Read sync")
 	return value
 }
