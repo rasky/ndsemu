288d9dabec4b87b3afdfefb2cc1c3ff71bfc680a
Giovanni Bajo <rasky@develer.com>
2015-12-17 03:53:25 +0100

    firmware: silent logs

--- firmware.go
+++ firmware.go
@@ -49,7 +49,7 @@ func (ff *HwFirmwareFlash) transfer(ch chan uint8) {
 			}
 			data := buf[0]
 			buf = buf[1:]
-			log.Infof("[firmware] reading: %02x", data)
+			// log.Infof("[firmware] reading: %02x", data)
 			ch <- data
 		}
 	}
