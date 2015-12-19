3365c58ffab42b93a04127b2296a9afc53657b3c
Giovanni Bajo <rasky@develer.com>
2015-12-19 17:41:31 +0100

    nds7: map empty GBA slot area

--- nds7.go
+++ nds7.go
@@ -30,9 +30,11 @@ func NewNDS7(ram []byte) *NDS7 {
 		MainRam: ram,
 	}
 
+	var zero [16]byte
 	bus.MapMemorySlice(0x00000000, 0x00003FFF, bios7, true)
 	bus.MapMemorySlice(0x02000000, 0x02FFFFFF, nds7.MainRam, false)
 	bus.MapMemorySlice(0x03800000, 0x03FFFFFF, nds7.WRam[:], false)
+	bus.MapMemorySlice(0x08000000, 0x09FFFFFF, zero[:], true)
 
 	return nds7
 }
