9a3df22e06df33096300bbb8283dc5253dfd35fe
Giovanni Bajo <rasky@develer.com>
2015-12-29 16:32:18 +0100

    io7: remove halt logging

--- io7.go
+++ io7.go
@@ -3,8 +3,6 @@ package main
 import (
 	"ndsemu/arm"
 	"ndsemu/emu/hwio"
-
-	log "gopkg.in/Sirupsen/logrus.v0"
 )
 
 type miscRegs7 struct {
@@ -83,7 +81,6 @@ func (m *NDS7IOMap) Write8(addr uint32, val uint8) {
 		m.Irq.WriteIME(uint16(val))
 	case 0x0301:
 		nds7.Cpu.SetLine(arm.LineHalt, true)
-		log.Warn("[io7] CPU halt")
 	default:
 		if addr < 0x4100000 {
 			m.TableLo.Write8(addr, val)
