6aed5275122548a3814d146b302baa5bf26af3bd
Giovanni Bajo <rasky@develer.com>
2016-01-03 03:15:31 +0100

    emu: add SwapNN functions

--- emu/hex.go
+++ emu/hex.go
@@ -1,6 +1,26 @@
 package emu
 
-import "fmt"
+import (
+	"encoding/binary"
+
+	"fmt"
+)
+
+func Swap16(val uint16) uint16 {
+	return (val >> 8) | (val << 8)
+}
+
+func Swap32(val uint32) uint32 {
+	var buf [4]byte
+	binary.LittleEndian.PutUint32(buf[:], val)
+	return binary.BigEndian.Uint32(buf[:])
+}
+
+func Swap64(val uint64) uint64 {
+	var buf [8]byte
+	binary.LittleEndian.PutUint64(buf[:], val)
+	return binary.BigEndian.Uint64(buf[:])
+}
 
 func Hex64(val uint64) string {
 	return fmt.Sprintf("%016x", val)
