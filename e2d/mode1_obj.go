5227965f04944237c0c30e4d80478c821db83bcc
Giovanni Bajo <rasky@develer.com>
2017-05-31 03:42:54 +0200

    e2d: fix object priority handling in NDS

--- e2d/mode1_obj.go
+++ e2d/mode1_obj.go
@@ -134,252 +134,259 @@ func (e2d *HwEngine2d) drawOBJ(lidx int, drawWindow bool) func(gfx.Line) {
 		// emulate the sprite line limits; the correct solution would be
 		// to go through in the correct order, but avoiding writing pixels
 		// that have been already written to.
-		for i := 127; i >= 0; i-- {
-			a0, a1, a2 := emu.Read16LE(oam[i*8:]), emu.Read16LE(oam[i*8+2:]), emu.Read16LE(oam[i*8+4:])
+		for wpri := 3; wpri >= 0; wpri-- {
+			for i := 127; i >= 0; i-- {
+				a0, a1, a2 := emu.Read16LE(oam[i*8:]), emu.Read16LE(oam[i*8+2:]), emu.Read16LE(oam[i*8+4:])
 
-			// Sprite mode: 0=normal, 1=affine, 2=hidden, 3=affine double
-			// We immediately skip hidden sprites, so from this point onward,
-			// mode!=0 means affine mode.
-			mode := (a0 >> 8) & 3
-			if mode == objModeHidden {
-				continue
-			}
-
-			// Sprite pixel mode: 0=normal, 1=semi-transparent, 2=window, 3=bitmap
-			pixmode := (a0 >> 10) & 3
-
-			// If it's an object window, draw it only if we were requested to
-			// do it. Otherwise, skip this sprite
-			if pixmode == objPixModeWindow {
-				if !drawWindow {
-					continue
-				}
-				pixmode = objPixModeNormal
-			} else {
-				if drawWindow {
-					continue
-				}
-			}
-
-			const XMask = 0x1FF
-			const YMask = 0xFF
-
-			x := int(a1 & XMask)
-			y := int(a0 & YMask)
-			if x >= cScreenWidth {
-				x -= XMask + 1
-			}
-			if y >= cScreenHeight {
-				y -= YMask + 1
-			}
-
-			// Get the object size. The size is expressed in number of chars,
-			// not pixels.
-			sz := objWidth[((a0>>14)<<2)|(a1>>14)]
-			tw, th := sz.w, sz.h
-			tws, ths := tw, th
-
-			if mode == objModeAffineDouble {
-				tws *= 2
-				ths *= 2
-			}
-
-			// If the sprite is visible
-			// FIXME: this doesn't handle wrapping yet
-			if sy >= y && sy < (y+ths*8) && (x < cScreenWidth && (x+tws*8) >= 0) {
-				tilenum := int(a2 & 1023)
-				depth256 := (a0>>13)&1 != 0
-				hflip := (a1>>12)&1 != 0 && mode == objModeNormal // hflip not available in affine mode
-				vflip := (a1>>13)&1 != 0 && mode == objModeNormal // vflip not available in affine mode
 				pri := (a2 >> 10) & 3
-				pal := (a2 >> 12) & 0xF
-
-				// Size of a char (in byte), depending on the color setting
-				charSize := 32
-				if depth256 {
-					charSize = 64
+				if int(pri) != wpri {
+					continue
 				}
 
-				// Compute the offset within VRAM of the current object (for
-				// now, its top-left pixel)
-				var vramOffset int
-				if pixmode == objPixModeBitmap {
-					vramOffset = vramBitmapCalcAddress(tilenum)
+				// Sprite mode: 0=normal, 1=affine, 2=hidden, 3=affine double
+				// We immediately skip hidden sprites, so from this point onward,
+				// mode!=0 means affine mode.
+				mode := (a0 >> 8) & 3
+				if mode == objModeHidden {
+					continue
+				}
+
+				// Sprite pixel mode: 0=normal, 1=semi-transparent, 2=window, 3=bitmap
+				pixmode := (a0 >> 10) & 3
+
+				// If it's an object window, draw it only if we were requested to
+				// do it. Otherwise, skip this sprite
+				if pixmode == objPixModeWindow {
+					if !drawWindow {
+						continue
+					}
+					pixmode = objPixModeNormal
 				} else {
-					vramOffset = tilenum * boundary
-				}
-
-				// Compute the line being drawn *within* the current object.
-				// This must also handle vertical flip (in which the whole
-				// object is flipped, not just the single chars)
-				y0 := (sy - y)
-				if vflip {
-					y0 = ths*8 - y0 - 1
-				}
-
-				// Calculate the pitch of a sprite, expressed in number of chars)
-				// This depends on the 1D vs 2D tile mapping in VRAM; 1D
-				// mapping means that tiles are arranged linearly in memory so the
-				// pitch is just the size of the sprite (in tiles).
-				// In 2D mapping, tiles are arranged in a 2D grid with a fixed size
-				// depending on the BPP, and thus not
-				pitch := tw
-				if pixmode == objPixModeBitmap {
-					if !bitmapMapping1d {
-						pitch = objPitch
-					}
-				} else {
-					if !mapping1d {
-						if depth256 {
-							pitch = 16
-						} else {
-							pitch = 32
-						}
+					if drawWindow {
+						continue
 					}
 				}
 
-				// See if we need to draw in affine mode
-				if mode != objModeNormal {
-					if pixmode == objPixModeBitmap {
-						panic("bitmap mode not supported in affine")
-					}
-					parms := ((a1>>9)&0x1F)*0x20 + 0x6
-					dx := int(int16(emu.Read16LE(oam[parms:])))
-					dmx := int(int16(emu.Read16LE(oam[parms+8:])))
-					dy := int(int16(emu.Read16LE(oam[parms+16:])))
-					dmy := int(int16(emu.Read16LE(oam[parms+24:])))
+				const XMask = 0x1FF
+				const YMask = 0xFF
 
-					sx := (tw*8/2)<<8 - (tws*8/2)*dx - (ths*8/2)*dmx + y0*dmx
-					sy := (th*8/2)<<8 - (tws*8/2)*dy - (ths*8/2)*dmy + y0*dmy
+				x := int(a1 & XMask)
+				y := int(a0 & YMask)
+				if x >= cScreenWidth {
+					x -= XMask + 1
+				}
+				if y >= cScreenHeight {
+					y -= YMask + 1
+				}
 
-					src := tiles.FetchPointer(vramOffset)
-					dst := line
+				// Get the object size. The size is expressed in number of chars,
+				// not pixels.
+				sz := objWidth[((a0>>14)<<2)|(a1>>14)]
+				tw, th := sz.w, sz.h
+				tws, ths := tw, th
 
-					attrs := uint32(pri) << 29
-					attrs |= (4 << 26) // layer=4 -> obj
-					if pixmode == objPixModeAlpha {
-						attrs |= 1 << 25
-					}
+				if mode == objModeAffineDouble {
+					tws *= 2
+					ths *= 2
+				}
+
+				// If the sprite is visible
+				// FIXME: this doesn't handle wrapping yet
+				if sy >= y && sy < (y+ths*8) && (x < cScreenWidth && (x+tws*8) >= 0) {
+					tilenum := int(a2 & 1023)
+					depth256 := (a0>>13)&1 != 0
+					hflip := (a1>>12)&1 != 0 && mode == objModeNormal // hflip not available in affine mode
+					vflip := (a1>>13)&1 != 0 && mode == objModeNormal // vflip not available in affine mode
+					pal := (a2 >> 12) & 0xF
+
+					// Size of a char (in byte), depending on the color setting
+					charSize := 32
 					if depth256 {
-						if useExtPal {
-							attrs |= uint32(pal<<8) | (1 << 12)
+						charSize = 64
+					}
+
+					// Compute the offset within VRAM of the current object (for
+					// now, its top-left pixel)
+					var vramOffset int
+					if pixmode == objPixModeBitmap {
+						vramOffset = vramBitmapCalcAddress(tilenum)
+					} else {
+						vramOffset = tilenum * boundary
+					}
+
+					// Compute the line being drawn *within* the current object.
+					// This must also handle vertical flip (in which the whole
+					// object is flipped, not just the single chars)
+					y0 := (sy - y)
+					if vflip {
+						y0 = ths*8 - y0 - 1
+					}
+
+					// Calculate the pitch of a sprite, expressed in number of chars)
+					// This depends on the 1D vs 2D tile mapping in VRAM; 1D
+					// mapping means that tiles are arranged linearly in memory so the
+					// pitch is just the size of the sprite (in tiles).
+					// In 2D mapping, tiles are arranged in a 2D grid with a fixed size
+					// depending on the BPP, and thus not
+					pitch := tw
+					if pixmode == objPixModeBitmap {
+						if !bitmapMapping1d {
+							pitch = objPitch
 						}
 					} else {
-						attrs |= uint32(pal << 4)
-						if useExtPal {
-							attrs |= (1 << 12)
-						}
-					}
-
-					for j := 0; j < tws*8; j++ {
-						if x >= 0 && x < cScreenWidth {
-							isx, isy := sx>>8, sy>>8
-							if isx >= 0 && isx < tw*8 && isy >= 0 && isy < th*8 {
-								ty := isy / 8
-								off := (pitch * charSize) * ty
-								isy &= 7
-
-								tx := isx / 8
-								off += charSize * tx
-								isx &= 7
-
-								if depth256 {
-									pix := uint32(src[off+isy*8+isx])
-									if pix != 0 {
-										dst.Set32(x, pix|attrs)
-									}
-								} else {
-									pix := uint32(src[off+isy*4+isx/2])
-									pix >>= 4 * uint(isx&1)
-									pix &= 0xF
-									if pix != 0 {
-										dst.Set32(x, pix|attrs)
-									}
-								}
+						if !mapping1d {
+							if depth256 {
+								pitch = 16
+							} else {
+								pitch = 32
 							}
 						}
-
-						sx += dx
-						sy += dy
-						x++
 					}
 
-				} else {
-					if pixmode == objPixModeBitmap {
+					// See if we need to draw in affine mode
+					if mode != objModeNormal {
+						if pixmode == objPixModeBitmap {
+							panic("bitmap mode not supported in affine")
+						}
+						parms := ((a1>>9)&0x1F)*0x20 + 0x6
+						dx := int(int16(emu.Read16LE(oam[parms:])))
+						dmx := int(int16(emu.Read16LE(oam[parms+8:])))
+						dy := int(int16(emu.Read16LE(oam[parms+16:])))
+						dmy := int(int16(emu.Read16LE(oam[parms+24:])))
 
-						vramOffset += (pitch * 8 * y0) * 2
+						sx := (tw*8/2)<<8 - (tws*8/2)*dx - (ths*8/2)*dmx + y0*dmx
+						sy := (th*8/2)<<8 - (tws*8/2)*dy - (ths*8/2)*dmy + y0*dmy
+
 						src := tiles.FetchPointer(vramOffset)
 						dst := line
 
-						attrs := (uint32(pri) << 29) | (4 << 26) | 0x80000000
-
-						// "pal" is reused as alpha. If zero -> transparent
-						if pal != 0 {
-							// Embed it in every pixel, in case
-							// we need to alpha blend. We extend it to 5 bits.
-							pal = pal<<1 + 1
-							attrs |= 1<<24 | uint32(pal)<<20
-
-							for j := 0; j < tw*8; j++ {
-								if x >= 0 && x < cScreenWidth {
-									var px uint32
-									if !hflip {
-										px = uint32(emu.Read16LE(src[j*2:]))
-									} else {
-										px = uint32(emu.Read16LE(src[(tw*8-j-1)*2:]))
-									}
-									if px&0x8000 != 0 {
-										dst.Set32(x, px|attrs)
-									}
-								}
-								x++
-							}
-						}
-
-					} else {
-						// Calculate the char row being drawn.
-						ty := y0 / 8
-
-						// Adjust the offset to the beginning of the correct char row
-						// within the object.
-						vramOffset += (pitch * charSize) * ty
-
-						// Now calculate the line being drawn within the current char row
-						y0 &= 7
-
-						// Prepare initial src/dst pointer for drawing
-						dst := line
-						dst.Add32(x)
-
-						attrs := (uint32(pri) << 29) | (4 << 26)
+						attrs := uint32(pri) << 29
+						attrs |= (4 << 26) // layer=4 -> obj
 						if pixmode == objPixModeAlpha {
 							attrs |= 1 << 25
 						}
-						for j := 0; j < tw; j++ {
-							var tsrc []byte
-							if !hflip {
-								tsrc = tiles.FetchPointer(vramOffset + charSize*j)
-							} else {
-								tsrc = tiles.FetchPointer(vramOffset + charSize*(tw-j-1))
+						if depth256 {
+							if useExtPal {
+								attrs |= uint32(pal<<8) | (1 << 12)
 							}
+						} else {
+							attrs |= uint32(pal << 4)
+							if useExtPal {
+								attrs |= (1 << 12)
+							}
+						}
 
-							if x > -8 && x < cScreenWidth {
-								if depth256 {
-									if !useExtPal {
-										pal = 0
+						for j := 0; j < tws*8; j++ {
+							if x >= 0 && x < cScreenWidth {
+								isx, isy := sx>>8, sy>>8
+								if isx >= 0 && isx < tw*8 && isy >= 0 && isy < th*8 {
+									ty := isy / 8
+									off := (pitch * charSize) * ty
+									isy &= 7
+
+									tx := isx / 8
+									off += charSize * tx
+									isx &= 7
+
+									if depth256 {
+										pix := uint32(src[off+isy*8+isx])
+										if pix != 0 {
+											dst.Set32(x, pix|attrs)
+										}
+									} else {
+										pix := uint32(src[off+isy*4+isx/2])
+										pix >>= 4 * uint(isx&1)
+										pix &= 0xF
+										if pix != 0 {
+											dst.Set32(x, pix|attrs)
+										}
 									}
-									e2d.drawChar256(y0, tsrc, dst, hflip, attrs, pal, useExtPal)
-								} else {
-									e2d.drawChar16(y0, tsrc, dst, hflip, attrs, pal, false)
 								}
 							}
-							dst.Add32(8)
-							x += 8
+
+							sx += dx
+							sy += dy
+							x++
+						}
+
+					} else {
+						if pixmode == objPixModeBitmap {
+
+							vramOffset += (pitch * 8 * y0) * 2
+							src := tiles.FetchPointer(vramOffset)
+							dst := line
+
+							attrs := (uint32(pri) << 29) | (4 << 26) | 0x80000000
+
+							// "pal" is reused as alpha. If zero -> transparent
+							if pal != 0 {
+								// Embed it in every pixel, in case
+								// we need to alpha blend. We extend it to 5 bits.
+								pal = pal<<1 + 1
+								attrs |= 1<<24 | uint32(pal)<<20
+
+								for j := 0; j < tw*8; j++ {
+									if x >= 0 && x < cScreenWidth {
+										var px uint32
+										if !hflip {
+											px = uint32(emu.Read16LE(src[j*2:]))
+										} else {
+											px = uint32(emu.Read16LE(src[(tw*8-j-1)*2:]))
+										}
+										if px&0x8000 != 0 {
+											dst.Set32(x, px|attrs)
+										}
+									}
+									x++
+								}
+							}
+
+						} else {
+							// Calculate the char row being drawn.
+							ty := y0 / 8
+
+							// Adjust the offset to the beginning of the correct char row
+							// within the object.
+							vramOffset += (pitch * charSize) * ty
+
+							// Now calculate the line being drawn within the current char row
+							y0 &= 7
+
+							// Prepare initial src/dst pointer for drawing
+							dst := line
+							dst.Add32(x)
+
+							attrs := (uint32(pri) << 29) | (4 << 26)
+							if pixmode == objPixModeAlpha {
+								attrs |= 1 << 25
+							}
+							for j := 0; j < tw; j++ {
+								var tsrc []byte
+								if !hflip {
+									tsrc = tiles.FetchPointer(vramOffset + charSize*j)
+								} else {
+									tsrc = tiles.FetchPointer(vramOffset + charSize*(tw-j-1))
+								}
+
+								if x > -8 && x < cScreenWidth {
+									if depth256 {
+										if !useExtPal {
+											pal = 0
+										}
+										e2d.drawChar256(y0, tsrc, dst, hflip, attrs, pal, useExtPal)
+									} else {
+										e2d.drawChar16(y0, tsrc, dst, hflip, attrs, pal, false)
+									}
+								}
+								dst.Add32(8)
+								x += 8
+							}
 						}
 					}
 				}
 			}
 		}
+
 		sy++
 	}
 }
