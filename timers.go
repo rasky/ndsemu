9b817c01d830d8ecd8a77cc43d994aa35d0a3eb6
Giovanni Bajo <rasky@develer.com>
2017-06-11 02:26:53 +0200

    timers: slight precision improvement

--- timers.go
+++ timers.go
@@ -48,16 +48,21 @@ func (t *HwTimer) reschedule() {
 	if t.running() && t.irq() {
 		ticks := 0x10000 - int(t.counter)
 		t.sync = t.cycles + int64(ticks*t.scaler())
-		// t.log().WithFields(logrus.Fields{
-		// 	"now":  Emu.Sync.Cycles(),
-		// 	"now2": t.cycles,
-		// 	"when": t.sync,
-		// }).Infof("scheduling sync")
+		// log.ModTimer.DebugZ("schedule sync").
+		// 	String("name", t.name).
+		// 	Int64("now", Emu.Sync.Cycles()).
+		// 	Int64("now2", t.cycles).
+		// 	Int64("when", t.sync).
+		// 	End()
 		Emu.Sync.ScheduleSync(t.sync)
 	}
 }
 
-func (t *HwTimer) WriteRELOAD(_, val uint16) {
+func (t *HwTimer) WriteRELOAD(old, val uint16) {
+	t.Reload.Value = old
+	t.Run(Emu.Sync.Cycles())
+	t.Reload.Value = val
+
 	log.ModTimer.InfoZ("write reload").
 		String("name", t.name).
 		Hex16("val", val).
@@ -69,6 +74,11 @@ func (t *HwTimer) WriteCONTROL(old, val uint16) {
 	wasrunning := t.running()
 
 	t.Run(Emu.Sync.Cycles())
+
+	log.ModTimer.InfoZ("write control").
+		String("name", t.name).
+		Hex16("val", val).
+		End()
 
 	t.Control.Value = val
 	if !wasrunning && t.running() {
@@ -141,6 +151,12 @@ func (t *HwTimer) Run(target int64) {
 		return
 	}
 
+	// log.ModTimer.DebugZ("Run").
+	// 	String("name", t.name).
+	// 	Int64("tgt", target).
+	// 	Int64("cyc", t.cycles).
+	// 	End()
+
 	scaler := int64(t.scaler())
 	elapsed := target - t.cycles
 	if elapsed < 0 {
@@ -186,6 +202,7 @@ func (t *HwTimers) Reset() {
 	for i := range t.Timers {
 		t.Timers[i] = HwTimer{}
 		hwio.MustInitRegs(&t.Timers[i])
+		t.Timers[i].Reload.WriteCb = t.Timers[i].WriteRELOAD
 		if i != 3 {
 			t.Timers[i].next = &t.Timers[i+1]
 		}
@@ -200,7 +217,17 @@ func (t *HwTimers) Cycles() int64 {
 	// All timers are (mostly) aligned, so just use any to return the
 	// current time. The only possible misalignment could be because
 	// of rounding errors
-	return t.Timers[0].cycles
+	c := t.Timers[0].cycles
+	if c > t.Timers[1].cycles {
+		c = t.Timers[1].cycles
+	}
+	if c > t.Timers[2].cycles {
+		c = t.Timers[2].cycles
+	}
+	if c > t.Timers[3].cycles {
+		c = t.Timers[3].cycles
+	}
+	return c
 }
 
 func (t *HwTimers) Run(target int64) {
