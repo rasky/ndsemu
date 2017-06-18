663bf66d6ee5c1dac6095ab1bb51e1a23633194b
Giovanni Bajo <rasky@develer.com>
2017-06-18 15:52:20 +0200

    Revert "dma: activate DMA channel even if it was already activated"
    This reverts commit f7dc747eb1f5772175442fb85ad27a2d1ab87cdc.

--- dma.go
+++ dma.go
@@ -134,7 +134,7 @@ func (dma *HwDmaChannel) WriteDMACNTRL(old, val uint16) {
 	// Check if this write activated a DMA channel. If it did,
 	// we might to do something right away, depending on the start
 	// event type.
-	if val&(1<<15) != 0 {
+	if ((old^val)>>15)&1 != 0 {
 		evt := dma.startEvent()
 		switch evt {
 		case DmaEventImmediate:
